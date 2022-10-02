cd3e44d5eaeb41844a03c6e5cd2cbbab4e1eb598
Mahmood Tahir <mahmood.tahir@thescore.com>
2022-10-01 22:39:32 -0400

    Do not call did move to parent to itself

--- Sources/SnapshotTesting/Common/View.swift
+++ Sources/SnapshotTesting/Common/View.swift
@@ -1074,11 +1074,11 @@
           ])
         }
         rootViewController.addChild(viewController)
+        viewController.didMove(toParent: rootViewController)
       } else {
         rootViewController = viewController
       }
       rootViewController.setOverrideTraitCollection(traits, forChild: viewController)
-      viewController.didMove(toParent: rootViewController)
 
       window.rootViewController = rootViewController
 
